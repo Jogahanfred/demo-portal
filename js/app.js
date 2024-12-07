5345f3606c9566e93572d5f8edb775b6722d833a
= <=>
2024-12-06 21:48:08 -0500

    Se guardo faltando corregir el sidebar pantalla grande

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+var typed = new Typed('#msgTyped', {
+  strings: [
+    'Revisa el PDE - Programa de Entrenamiento (Solo para instructores)',
+    'Cuenta con PDI - Programa de Instrucción (Dirigido a Alumnos Pilotos)',
+    'Consulta tus horarios actualizados.',
+    'Revisa las novedades en tu portal!',
+  ],
+  typeSpeed: 70,
+  loop: false,
+  loopCount: Infinity,
+});
+
+const showNavbar = () => {
+  const svgHamburger = document.getElementById("svg-hamburger");
+  const svgXMark = document.getElementById("svg-x-mark");
+  const navbar = document.querySelector(".menunav");
+
+  if (svgXMark.classList.contains("hidden")) {
+    navbar.classList.remove("hidden");
+    svgHamburger.classList.add("hidden");
+    svgXMark.classList.remove("hidden");
+  } else {
+    navbar.classList.add("hidden");
+    svgXMark.classList.add("hidden");
+    svgHamburger.classList.remove("hidden");
+  }
+};
+
+const btnHamburger = document.getElementById("btnHamburger");
+btnHamburger.addEventListener("click", showNavbar);
+
 const btnPlusCourse1 = document.getElementById("btn-plus-course-1");
 
 const showContentDetailCourse1 = () => {
@@ -41,50 +72,130 @@ const showContentDetailCourse1 = () => {
 
 btnPlusCourse1.addEventListener("click", showContentDetailCourse1);
 
-const menuItems = document.querySelectorAll('.menu-item');
-menuItems.forEach(item => { 
-    const subMenu = item.querySelector('.sub-nav-item');
- 
-    item.addEventListener('mouseover', () => { 
-        subMenu.classList.remove('hidden');  
-    });
+const menuItems = document.querySelectorAll(".menu-item");
 
-    item.addEventListener('mouseout', () => { 
-        subMenu.classList.add('hidden');  
-    }); 
+let isLargeScreen = window.matchMedia("(min-width: 1024px)").matches; 
 
-    
-    const menuLink = item.querySelector('a');
-    menuLink.addEventListener('click', (e) => {
-        e.preventDefault(); 
-
-        menuItems.forEach(i => {
-            const link = i.querySelector('a');
-            link.classList.remove('selected-menu');
-        });
-
-        menuLink.classList.add('selected-menu');
-    });
-    
-  const subMenuLinks = subMenu.querySelectorAll('a');
-  subMenuLinks.forEach(subMenuLink => {
-      subMenuLink.addEventListener('click', (e) => {
-          e.preventDefault(); 
- 
-          menuItems.forEach(i => {
-              const link = i.querySelector('a');
-              link.classList.remove('selected-menu');
-          });
- 
-          menuLink.classList.add('selected-menu');
+function handleMenuItems() {
+  // Si la pantalla es pequeña, no hacemos nada
+  if (!isLargeScreen) {
+    // Eliminar los eventos si es pantalla pequeña
+    menuItems.forEach((item) => {
+      const subMenu = item.querySelector(".sub-nav-item");
+      item.removeEventListener("mouseover", showSubMenu);
+      item.removeEventListener("mouseout", hideSubMenu);
+      
+      const menuLink = item.querySelector("a");
+      
+      menuLink.removeEventListener("click", selectMenuItem);
+      const subMenuLinks = subMenu.querySelectorAll("a");
+      subMenuLinks.forEach((subMenuLink) => {
+        subMenuLink.removeEventListener("click", selectMenuItem);
       });
+
+
+      item.querySelector(".group").addEventListener("click", toggleSubMenu);
+
+      
+    });
+    return;
+  }
+
+  // Si es pantalla grande, añadimos los eventos
+  menuItems.forEach((item) => {
+    const subMenu = item.querySelector(".sub-nav-item");
+
+    item.addEventListener("mouseover", showSubMenu);
+    item.addEventListener("mouseout", hideSubMenu);
+
+    const menuLink = item.querySelector("a");
+    menuLink.addEventListener("click", selectMenuItem);
+
+    const subMenuLinks = subMenu.querySelectorAll("a");
+    subMenuLinks.forEach((subMenuLink) => {
+      subMenuLink.addEventListener("click", selectMenuItem);
+    });
+
+
+
+  });
+}
+
+// Función para mostrar el submenú
+function showSubMenu() {
+  const subMenu = this.querySelector(".sub-nav-item");
+  subMenu.classList.remove("hidden");
+}
+
+// Función para ocultar el submenú
+function hideSubMenu() {
+  const subMenu = this.querySelector(".sub-nav-item");
+  subMenu.classList.add("hidden");
+}
+
+function toggleSubMenu(e) {
+  e.preventDefault(); // Prevenir el comportamiento de navegación si es necesario
+
+  const menuItem = this.closest(".menu-item"); 
+  const divContent = menuItem.querySelector("div"); 
+
+  menuItems.forEach((i) => {
+    const otherDivContent = i.querySelector("div"); 
+    if (i !== menuItem) {
+      otherDivContent.classList.remove('bg-fap-red', 'text-white');
+    }
+  });
+
+  // Agregar las clases 'bg-fap-red' y 'text-white' al item seleccionado
+  divContent.classList.add('bg-fap-red', 'text-white');  
+
+
+  const subMenu = menuItem.querySelector(".sub-nav-item");
+  
+  // Alternar la clase 'hidden' en el submenú correspondiente
+  subMenu.classList.toggle("hidden");
+
+  // Cerrar otros submenús al hacer clic en uno
+  menuItems.forEach((i) => {
+    if (i !== this.closest(".menu-item")) {
+      const otherSubMenu = i.querySelector(".sub-nav-item");
+      // divContent.classList.remove('bg-fap-red', 'text-white') 
+      otherSubMenu.classList.add("hidden");
+    }
+  });
+}
+
+// Función para manejar la selección del menú
+function selectMenuItem(e) {
+  e.preventDefault(); // Evitar el comportamiento por defecto del enlace
+  
+  // Eliminar la clase 'selected-menu' de todos los enlaces
+  menuItems.forEach((item) => {
+    const link = item.querySelector("span");
+    link.classList.remove("selected-menu");
+  });
+
+  // Agregar la clase 'selected-menu' al enlace clicado
+  const menuLink = e.target;
+  menuLink.classList.add("selected-menu");
+}
+
+
+// Inicializamos los eventos de acuerdo al tamaño de la pantalla
+handleMenuItems();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const firstMenuLink = document.querySelector('a[href=""]');
+  if (firstMenuLink) {
+    firstMenuLink.classList.add("selected-menu");
+  }
+
+  // Detectar cambio de tamaño
+  window.addEventListener("resize", () => {
+    const currentIsLargeScreen = window.matchMedia("(min-width: 1024px)").matches;
+    if (isLargeScreen !== currentIsLargeScreen) {
+      isLargeScreen = currentIsLargeScreen; // Actualizar el tamaño de la pantalla
+      handleMenuItems(); // Volver a configurar los eventos
+    }
   });
 });
- 
-document.addEventListener('DOMContentLoaded', () => {
-    const firstMenuLink = document.querySelector('a[href=""]'); 
-    if (firstMenuLink) {
-        firstMenuLink.classList.add('selected-menu');
-    }
-});
-
